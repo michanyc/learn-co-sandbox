a99443d2e41ce71e62fec9ce6c86da9de71acd01
Michael Gerland <mr.gerland@gmail.com>
2019-10-23 17:16:04 +0000

    Automatically backed up by Learn

---
+++ case.rb
@@ -0,0 +1,20 @@
+=begin
+greeting = "friendly_greeting"
+case greeting
+  when "unfriendly_greeting"
+    puts "What do you want?!"
+  when "friendly_greeting"
+    puts "Hi, how are you?"
+end
+=end
+
+current_weather = "snowing"
+
+case current_weather
+  when "sunny"
+    puts "grab some sunscreen"
+  when "raining"
+    puts "grab an umbrella"
+  when "snowing"
+    puts "bundle up"
+end
---
+++ var.rb
@@ -0,0 +1,12 @@
+=begin
+n = 2
+count = 0
+while count <= n do
+  puts "I ran"
+  count += 1
+end
+=end
+
+3.times do
+  puts "I ran again"
+end
